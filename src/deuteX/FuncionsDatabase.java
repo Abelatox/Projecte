d7beb70a81a6296269bd0564d20cda4d9f10cb3c
Unknown <smxxcastillo@gmail.com>
2017-03-17 17:09:45 +0100

    Comentaris de FuncionsDatabase

--- src/deuteX/FuncionsDatabase.java
+++ src/deuteX/FuncionsDatabase.java
@@ -6,38 +6,49 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class FuncionsDatabase {
-
+	
+//Comprova si l'usuari ja està creat a la base de dades. 
 	public static boolean existeixUsuari(Connection conn, String usuari){
+		//Si la connexió no és nul·la
 		if(conn != null)
 	    {
 			try {
 				Statement st = conn.createStatement();
 				ResultSet rs = st.executeQuery("select * from usuaris");
+				
 				while(rs.next())
+					//Comprova si l'usuari està a la Base de Dades
 				if(usuari.equals(rs.getString("nom"))){
+					//Retorna un valor cert perquè pugui continuar.
 					return true;
 				}
-				
+				//Tanquem les connexions a la base de dades.
 				rs.close();
 				st.close();
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
 	    }
+		//Retorna un valor fals perquè l'usuari no existeix.
 		return false;
 	}
 	
+	//Comprova el login d'entrada si l'usuari i la contrasenya estan a la base de dades i són correctes
 	public static boolean validarLogin(Connection conn, String usuari, String password){
+		//Si la connexió no és nul·la
 		if(conn != null)
 	    {
 			try {
 				Statement st = conn.createStatement();
 				ResultSet rs = st.executeQuery("select * from usuaris");
+				
 				while(rs.next())
+				//Si el nom i la contrasenya són correctes.
 				if(usuari.equals(rs.getString("nom")) && password.equals(rs.getString("pass"))){
+					//Retorna un valor cert.
 					return true;
 				}
-				
+				//Tanquem les connexions a la base de dades.
 				rs.close();
 				st.close();
 			} catch (SQLException e) {
@@ -53,6 +64,7 @@ public class FuncionsDatabase {
 		try {
 			st = conn.createStatement();
 			ResultSet rs = st.executeQuery(" select * from usuaris u where u.nom like '"+usuari+"' ");
+			
 			while(rs.next())
 			{	
 				if(rs.getString("id_usuari") != null){
@@ -88,6 +100,7 @@ public class FuncionsDatabase {
 			String dbDeutor="";
 			st = conn.createStatement();
 			ResultSet rs = st.executeQuery(" select * from usuaris u left join deutes d on u.id_usuari=d.prestamista");
+			
 			while(rs.next()){
 				if(rs.getString("id_usuari").equals(rs.getString("prestamista"))){
 					if(rs.getString("deutor").equals(deutor)){
@@ -96,15 +109,14 @@ public class FuncionsDatabase {
 					}
 				}
 			}
-			System.out.println("(deutor'"+deutor+"','quantitat"+quantitat+"','usuariID"+usuariID+"')");
+			//System.out.println("(deutor'"+deutor+"','quantitat"+quantitat+"','usuariID"+usuariID+"')");
 
 			//Comprovem si el deutor ja existeix.
 			if(prestamistaID == usuariID && dbDeutor.equals(deutor)){
-				st.execute(" UPDATE deutes SET quantitat = quantitat + "+quantitat + " where prestamista = '"+usuariID+"' and deutor = '" + deutor+"' " );
+				st.execute("UPDATE deutes SET quantitat = quantitat + "+quantitat + " where prestamista = '"+usuariID+"' and deutor = '" + deutor+"' " );
 			}else{
 				st.execute(" INSERT INTO deutes (deutor,quantitat,prestamista) VALUES ('"+deutor+"','"+quantitat+"','"+usuariID+"') ");
 			}
-			//S'eliminen les deuded saldades
 			eliminarSaldades(conn);
 		}catch (SQLException e){
 			e.printStackTrace();
